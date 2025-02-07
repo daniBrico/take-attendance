96ae4c6a46ce458440bd44b3bfd1ebcc4aa3990a
Daniel <daniel.jorge96@outlook.com>
2025-02-07 15:36:55 -0300

    update tailwindcss to v4

--- client/src/components/TakeAttendanceButton.jsx
+++ client/src/components/TakeAttendanceButton.jsx
@@ -7,7 +7,7 @@ function TakeAttendanceButton({ userType, takeAttendance }) {
     <div className='flex'>
       <button
         type='button'
-        className='mx-auto rounded-md bg-gradient-to-r from-green-400 to-blue-500 px-4 py-2 font-semibold text-white transition-all duration-300 hover:from-green-500 hover:to-blue-600'
+        className='mx-auto rounded-md bg-linear-to-r from-green-400 to-blue-500 px-4 py-2 font-semibold text-white transition-all duration-300 hover:from-green-500 hover:to-blue-600'
         onClick={takeAttendance}
       >
         Tomar lista
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -47,7 +47,7 @@ function LoginPage() {
         )}
         <button
           type='submit'
-          className='w-full rounded-md bg-gradient-to-r from-green-400 to-blue-500 px-4 py-2 font-semibold text-white transition-all duration-300 hover:from-green-500 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
+          className='w-full rounded-md bg-linear-to-r from-green-400 to-blue-500 px-4 py-2 font-semibold text-white transition-all duration-300 hover:from-green-500 hover:to-blue-600 focus:outline-hidden focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
         >
           Login
         </button>
